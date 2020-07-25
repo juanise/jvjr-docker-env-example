559a8a0b59cea00968275f28b43215522252b78d
Juan Nicolau Sepúlveda <jnicolau@iti.es>
2020-07-25 09:09:44 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import EnvProvider from 'jvjr-docker-env'
 
 function App() {
+  const first = process.env.REACT_APP_FIRST;
+  const first2 = EnvProvider.value('FIRST');
+  const second = EnvProvider.value('SECOND');
+  const third = EnvProvider.value('THIRD');
   return (
     <div className="App">
       <header className="App-header">
@@ -10,14 +15,18 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          REACT_APP_FIRST env var: {first}
+        </p>
+        <p>
+          FIRTS EnvProvider: {first2}
+        </p>
+        <p>
+          SECOND EnvProvider changing default: {second}
+        </p>
+        <p>
+          This one do not start with REACT_APP so do not have default value: {third}
+        </p>
       </header>
     </div>
   );
